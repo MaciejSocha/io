53cb5f9edbb2677b0403098203e39c24210620af
Florke07 <maciek.socha1@gmail.com>
2018-12-18 19:47:29 +0100

    added maven

---
+++ src/main/java/importdata/JsonRead.java
@@ -0,0 +1,12 @@
+package importdata;
+
+import jdk.nashorn.internal.parser.JSONParser;
+
+public class JsonRead {
+    private String fileName;
+
+    public JsonRead(String file) {
+        fileName = file;
+    }
+
+}
